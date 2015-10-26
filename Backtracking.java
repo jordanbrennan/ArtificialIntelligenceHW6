97629db8ff26b2e190b2c693c84d460ef7a4e5ee
jordanbrennan <jordanbrennan21@gmail.com>
2015-10-26 14:49:29 -0500

    Initial commit

---
+++ Backtracking.java
@@ -0,0 +1,138 @@
+import java.util.Arrays;
+
+
+public class Backtracking {
+
+    private char[] assignment;
+    private int[] degrees;
+    private int[] mrv;
+    private char[] values;
+
+    public Backtracking(int[][] constraints, char[] values) {
+        assignment = new char[constraints.length];
+        Arrays.fill(assignment, 'Z');
+
+        //find number of degrees
+        degrees = new int[constraints.length];
+        Arrays.fill(degrees, 0);
+        for (int i = 0; i < constraints.length; i++) {
+            for (int j = 0; j < constraints.length; j++) {
+                if (constraints[i][j] == 1) degrees[i]++;
+            }
+        }
+
+        mrv = new int[constraints.length];
+        Arrays.fill(mrv, values.length);
+
+        this.values = values;
+    }
+
+    /**
+     * Starts backtracking search
+     * @param constraints read from file
+     */
+    public void BacktrackingSearch(int[][] constraints) {
+        long startTime = System.nanoTime();
+
+        //start search and print results if solution found
+        if (RecursiveBacktracking(constraints)) {
+            System.out.println("Solution: ");
+            System.out.println(Arrays.toString(assignment));
+        }
+        else System.out.println("Failure");
+
+        long stopTime = System.nanoTime();
+        System.out.println("Time: " + ((stopTime - startTime)/1000000.00));
+    }
+
+    /**
+     * implements backtracking search
+     * @param constraints
+     * @return true/false success
+     */
+    private boolean RecursiveBacktracking( int[][] constraints) {
+
+        boolean complete = true;
+
+        //if assignment is complete, end
+        for (int i = 0; i < assignment.length; i++)
+            if (assignment[i] == 'Z') {
+                complete = false;
+                break;
+            }
+        if (complete) return true;
+
+        //select unassigned variable
+        int variable = SelectUnassignedVariable();
+
+        //iterate through values
+        for (int i = 0; i < values.length; i++) {
+
+            //if value is consistent
+            if (ValueIsConsistent(values[i], variable, constraints)) {
+
+                //add value to variable in assignment
+                assignment[variable] = values[i];
+
+                System.out.println("Variable: " + variable + " <- Value: " + values[i]);
+
+                //proceed to next state using recursion, if success then return
+                if (RecursiveBacktracking(constraints)) return true;
+
+                //remove value from assignment
+                assignment[variable] = 'Z';
+            }
+        }
+        //return failure
+        return false;
+    }
+
+    /**
+     * selects an unassigned variable with MRV and degree heuristics
+     * @return the variable
+     */
+    private int SelectUnassignedVariable() {
+
+        int variable = 0;
+        boolean init = true;
+
+        //loop through variables
+        for (int i = 0; i < assignment.length; i++) {
+            //select only if variable is unassigned
+            if (assignment[i] == 'Z') {
+                //initialize to first variable processed
+                if (init) {
+                    variable = i;
+                    init = false;
+                }
+                //check MRV heuristic
+                if (mrv[i] < mrv[variable]) variable = i;
+                else if (mrv[i] == mrv[variable])
+                    //check degree heuristic
+                    if (degrees[i] < degrees[variable]) variable = i;
+            }
+        }
+
+        return variable;
+    }
+
+    /**
+     * Checks if value is consistent with assignment
+     * @param value to be assigned
+     * @param variable to be assigned
+     * @param constraints
+     * @return
+     */
+    private boolean ValueIsConsistent(int value, int variable, int[][]constraints) {
+
+        //iterate through variables
+        for (int i = 1; i < constraints.length; i++) {
+            //check if variable has connection with same value
+            if (constraints[variable][i] == 1 && assignment[i] == value)
+                return false;
+        }
+
+        return true;
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //ensure argument contains both file names
+        if (args.length != 1) {
+            System.out.println("Incorrect number of arguments.");
+            return;
+        }
+
+        //contains matrix of constraints between variables
+        int[][] constraints;
+
+        try {
+            //get data from files
+            constraints = readFile(args[0]);
+
+            //for problem one, add 'd' for problem two
+            char[] values = {'a', 'b', 'c'};
+            Backtracking BTS = new Backtracking(constraints, values);
+
+            //implement backtracking search
+            BTS.BacktrackingSearch(constraints);
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * Reads content of files
+     * @param file - name of file to read from
+     * @return matrix containing contents of file
+     * @throws IOException
+     */
+    private static int[][] readFile( String file) throws IOException {
+        BufferedReader reader = new BufferedReader( new FileReader(file));
+        String line = null;
+        ArrayList<String> lines = new ArrayList<String>();
+
+        //traverse through file until end
+        while( ( line = reader.readLine() ) != null ) {
+            lines.add(line);
+        }
+
+        //process contents of lines then return it
+        return processFile(lines);
+    }
+
+    /**
+     * Processes content of files
+     * @param lines - string array list of lines from file
+     * @return matrix containing contents of lines
+     */
+    private static int[][] processFile(ArrayList<String> lines) {
+
+        //set number of columns
+        int columns = lines.get(0).split("\\t").length;
+
+        int[][] matrix = new int[lines.size()][columns];
+        String[] line;
+
+        //traverse through matrix and assign int value
+        for (int i = 0; i < lines.size(); i++) {
+            //get an array of values from the line
+            line = lines.get(i).split("\\t");
+            for (int j= 0; j < columns; j++) {
+                //convert to int and assign to matrix
+                matrix[i][j] = Integer.parseInt(line[j].trim());
+            }
+        }
+
+        return matrix;
+    }
+
+}
+
+
+
